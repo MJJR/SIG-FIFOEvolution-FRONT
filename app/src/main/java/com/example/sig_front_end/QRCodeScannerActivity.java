e38bb92fac42493fa0ebf1b3cc5d6beecbfef071
MΔЯĨИ ШĀŦĪ B <M  B>
2020-11-24 16:43:34 +0100

    qrcode scanner

---
+++ app/src/main/java/com/example/sig_front_end/QRCodeScannerActivity.java
@@ -0,0 +1,39 @@
+package com.example.sig_front_end;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Toast;
+
+import com.google.zxing.Result;
+import me.dm7.barcodescanner.zxing.ZXingScannerView;
+
+public class QRCodeScannerActivity extends AppCompatActivity implements ZXingScannerView.ResultHandler {
+
+    private ZXingScannerView mScannerView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mScannerView = new ZXingScannerView(this);
+        setContentView(mScannerView);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mScannerView.setResultHandler(this);
+        mScannerView.startCamera();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mScannerView.stopCamera();
+    }
+
+    @Override
+    public void handleResult(Result result) {
+        Toast.makeText(this,result.getText(),Toast.LENGTH_LONG).show();
+    }
+}
