5de3f5a511fd7e37be74fb8b15e359f5348e83c8
Le Henaff Antoine <antoine.le-henaff@etu.univ-orleans.fr>
2020-12-09 16:35:56 +0100

    ajout de la demande d'autorisation de Internet, Network Access et Wifi dans le manifest
    Ajout de Volley dans le builde.gradle pour faire les get/post
    
    Ajout de get/post dans la main activity + tentative de faire la demande de permission qui a pas super bien marché :)

--- app/src/main/java/com/example/sig_front_end/MainActivity.java
+++ app/src/main/java/com/example/sig_front_end/MainActivity.java
@@ -1,14 +1,20 @@
 package com.example.sig_front_end;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
+import androidx.appcompat.app.AppCompatActivity;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 
 //Splash screen et check des permissions
 public class MainActivity extends AppCompatActivity {
@@ -21,6 +27,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        GetListRDC();
+
         buttonScanner = (Button) findViewById(R.id.button_scanner);
         buttonPermissions = (Button) findViewById(R.id.button_permissions);
 
@@ -28,9 +36,13 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 int hasCameraPermission = checkSelfPermission(Manifest.permission.CAMERA);
+                int hasInternetPermission = checkSelfPermission(Manifest.permission.INTERNET);
+
                 if(hasCameraPermission == PackageManager.PERMISSION_GRANTED){
-                    Intent i = new Intent(MainActivity.this, QRCodeScannerActivity.class);
-                    startActivity(i);
+                    if(hasInternetPermission == PackageManager.PERMISSION_GRANTED) {
+                        Intent i = new Intent(MainActivity.this, QRCodeScannerActivity.class);
+                        startActivity(i);
+                    }
                 }
                 else {
                     makeText("Please enable permissions.");
@@ -42,14 +54,62 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 int hasCameraPermission = checkSelfPermission(Manifest.permission.CAMERA);
+                int hasInternetPermission = checkSelfPermission(Manifest.permission.INTERNET);
                 if(hasCameraPermission == PackageManager.PERMISSION_GRANTED){
-                    makeText("All required permissions are granted.");
-                }
-                else {
+                    if(hasInternetPermission == PackageManager.PERMISSION_GRANTED) {
+                        //makeText("All required permissions are granted.");
+                    }
+                } else {
                     askForPermission();
                 }
             }
         });
+    }
+
+    private void GetListRDC (){
+        RequestQueue requestQueue= Volley.newRequestQueue(this);
+        String URL="http://192.168.1.44:8080/rdc/rdcList";
+
+        StringRequest stringRequest =new StringRequest(
+                Request.Method.GET,
+                URL,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        makeText("Response : "+ response);
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.e("Err Response",error.toString());
+                    }
+                }
+        );
+        requestQueue.add(stringRequest);
+    }
+
+    private void Post (){
+        RequestQueue requestQueue= Volley.newRequestQueue(this);
+        String URL="http://192.168.1.44:8080/request";
+
+        StringRequest stringRequest = new StringRequest(
+                Request.Method.POST,
+                URL,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+
+                    }
+                }
+        );
+        requestQueue.add(stringRequest);
     }
 
     private void makeText(String text){
@@ -58,7 +118,9 @@ public class MainActivity extends AppCompatActivity {
 
     private void askForPermission(){
         final int REQUEST_CODE_ASK_PERMISSIONS = 123;
+        //final int REQUEST_CODE_ASK_PERMISSIONS2 = 124;
         requestPermissions(new String[] {Manifest.permission.CAMERA}, REQUEST_CODE_ASK_PERMISSIONS);
+        //requestPermissions(new String[] {Manifest.permission.INTERNET}, REQUEST_CODE_ASK_PERMISSIONS2);
     }
 
 }
