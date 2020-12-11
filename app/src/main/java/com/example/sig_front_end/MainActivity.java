5ecc66fdccbe895997e67b1d758ae04c41f1e106
Le Henaff Antoine <antoine.le-henaff@etu.univ-orleans.fr>
2020-12-11 18:10:38 +0100

    Le Scanner de QR code retourne vrai ou faux si le code scanné existe dans la base ou non. le main activity ne possède plus de GetListQRCode, elle a été déplacée dans l'activity du scanner !

--- app/src/main/java/com/example/sig_front_end/MainActivity.java
+++ app/src/main/java/com/example/sig_front_end/MainActivity.java
@@ -22,12 +22,11 @@ public class MainActivity extends AppCompatActivity {
     private Button buttonScanner;
     private Button buttonPermissions;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        GetListRDC();
 
         buttonScanner = (Button) findViewById(R.id.button_scanner);
         buttonPermissions = (Button) findViewById(R.id.button_permissions);
@@ -57,7 +56,7 @@ public class MainActivity extends AppCompatActivity {
                 int hasInternetPermission = checkSelfPermission(Manifest.permission.INTERNET);
                 if(hasCameraPermission == PackageManager.PERMISSION_GRANTED){
                     if(hasInternetPermission == PackageManager.PERMISSION_GRANTED) {
-                        //makeText("All required permissions are granted.");
+                        makeText("All required permissions are granted.");
                     }
                 } else {
                     askForPermission();
@@ -66,61 +65,13 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-    private void GetListRDC (){
-        RequestQueue requestQueue= Volley.newRequestQueue(this);
-        String URL="http://192.168.1.44:8080/rdc/rdcList";
-
-        StringRequest stringRequest =new StringRequest(
-                Request.Method.GET,
-                URL,
-                new Response.Listener<String>() {
-                    @Override
-                    public void onResponse(String response) {
-                        makeText("Response : "+ response);
-                    }
-                },
-                new Response.ErrorListener() {
-                    @Override
-                    public void onErrorResponse(VolleyError error) {
-                        Log.e("Err Response",error.toString());
-                    }
-                }
-        );
-        requestQueue.add(stringRequest);
-    }
-
-    private void Post (){
-        RequestQueue requestQueue= Volley.newRequestQueue(this);
-        String URL="http://192.168.1.44:8080/request";
-
-        StringRequest stringRequest = new StringRequest(
-                Request.Method.POST,
-                URL,
-                new Response.Listener<String>() {
-                    @Override
-                    public void onResponse(String response) {
-
-                    }
-                },
-                new Response.ErrorListener() {
-                    @Override
-                    public void onErrorResponse(VolleyError error) {
-
-                    }
-                }
-        );
-        requestQueue.add(stringRequest);
-    }
-
     private void makeText(String text){
         Toast.makeText(this,text,Toast.LENGTH_LONG).show();
     }
 
     private void askForPermission(){
         final int REQUEST_CODE_ASK_PERMISSIONS = 123;
-        //final int REQUEST_CODE_ASK_PERMISSIONS2 = 124;
         requestPermissions(new String[] {Manifest.permission.CAMERA}, REQUEST_CODE_ASK_PERMISSIONS);
-        //requestPermissions(new String[] {Manifest.permission.INTERNET}, REQUEST_CODE_ASK_PERMISSIONS2);
     }
 
 }
--- app/src/main/java/com/example/sig_front_end/QRCodeScannerActivity.java
+++ app/src/main/java/com/example/sig_front_end/QRCodeScannerActivity.java
@@ -3,18 +3,32 @@ package com.example.sig_front_end;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.google.zxing.Result;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import me.dm7.barcodescanner.zxing.ZXingScannerView;
 
 public class QRCodeScannerActivity extends AppCompatActivity implements ZXingScannerView.ResultHandler {
 
     private ZXingScannerView mScannerView;
+    private String list;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        GetListQRCode();
         mScannerView = new ZXingScannerView(this);
         setContentView(mScannerView);
     }
@@ -34,6 +48,44 @@ public class QRCodeScannerActivity extends AppCompatActivity implements ZXingSca
 
     @Override
     public void handleResult(Result result) {
-        Toast.makeText(this,result.getText(),Toast.LENGTH_LONG).show();
+        String QrScanned = result.getText();
+        exist(QrScanned);
+    }
+
+    private void GetListQRCode (){
+        RequestQueue requestQueue= Volley.newRequestQueue(getApplicationContext());
+        String URL="http://192.168.1.44:8080/qrcode/qrcodeList";
+
+        StringRequest stringRequest =new StringRequest(
+                Request.Method.GET,
+                URL,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        //makeText("Response : "+ response);
+                        list = response;
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.e("Err Response",error.toString());
+                    }
+                }
+        );
+        requestQueue.add(stringRequest);
+    }
+
+    private void exist(String text){
+        boolean bool = list.contains(text);
+        if(bool){
+            makeText("il y est");
+        }else{
+            makeText("il y est PAS");
+        }
+    }
+
+    private void makeText(String text){
+        Toast.makeText(this,text,Toast.LENGTH_LONG).show();
     }
 }
